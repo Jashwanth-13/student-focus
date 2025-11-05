12f341a9fa0f19488afeb5e74f58c81a64dde3b6
Jashwanth-13 <24881A0550@student.vardhaman.org>
2025-11-05 23:14:07 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(e){
+    // For demo: Just continue to index.html
+});
+
+document.getElementById('gmailLogin').addEventListener('click', function() {
+  alert('Gmail Login integration to be implemented.');
+});
+
+document.getElementById('forgotPass').addEventListener('click', function(){
+  alert('Password reset feature coming soon.');
+});
+
+document.getElementById('signUpLink').addEventListener('click', function(){
+  alert('Sign-up feature coming soon.');
+});
